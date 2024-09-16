1a2695b0001594ad3dfc177bb8249548b94cd445
Sameer Yadav <ysameer0026@gmail.com>
2024-09-16 05:43:42 +0530

    error fix

--- src/Pages/Video/Video11.jsx
+++ src/Pages/Video/Video11.jsx
@@ -1,5 +1,5 @@
 //import React from 'react'
-import PlayVideo11 from "../../Components/PlayVideo/Playvideo11"
+import PlayVideo11 from "../../Components/PlayVideo/PlayVideo11"
 const Video11 = () => {
   return (
     <div className='play-container'>
